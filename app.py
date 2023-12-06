ee9c5ad088f170b6bcb067e4a3e545ec2de5fe1c
bharani surya s <61204330+BharaniSurya@users.noreply.github.com>
2023-12-06 19:46:58 +1100

    Update app.py

--- app.py
+++ app.py
@@ -8,22 +8,9 @@ app = Flask(__name__)
 # Load your trained machine learning model
 model = joblib.load('random_forest_model.pkl')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    try:
-        # Get data from POST request
-        data = request.json
-        
-        # Convert data to numpy array for prediction
-        input_data = np.array(list(data.values())).reshape(1, -1)
-        
-        # Make prediction using the loaded model
-        prediction = model.predict(input_data)
-        
-        # Prepare the prediction result
-        output = {'prediction': int(prediction[0])}  # Assuming it's a single value prediction
-        
-        return jsonify(output)
+@app.route('/')
+def hello():
+    return 'Hello, World!'
     
     except Exception as e:
         return jsonify({'error': str(e)})
