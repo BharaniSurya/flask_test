990de4e093a8fedeab2ed82c52d589273c313c1c
bharani surya s <61204330+BharaniSurya@users.noreply.github.com>
2023-12-06 18:32:16 +1100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+import sklearn
+
+app = Flask(__name__)
+
+# Load your trained machine learning model
+model = joblib.load('random_forest_model.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Get data from POST request
+        data = request.json
+        
+        # Convert data to numpy array for prediction
+        input_data = np.array(list(data.values())).reshape(1, -1)
+        
+        # Make prediction using the loaded model
+        prediction = model.predict(input_data)
+        
+        # Prepare the prediction result
+        output = {'prediction': int(prediction[0])}  # Assuming it's a single value prediction
+        
+        return jsonify(output)
+    
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
